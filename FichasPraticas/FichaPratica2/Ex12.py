c3866bdf3e8a7e8cf456dffb0d936d7492c2ddad
CyberBitWizard <luismcmateusreis@gmail.com>
2024-09-16 15:24:47 +0100

    altered ficha 2 ex12

--- FichasPraticas/FichaPratica2/Ex12.py
+++ FichasPraticas/FichaPratica2/Ex12.py
@@ -1,3 +1,7 @@
+print("Menu\n1. Criar\n2. Atualizar \n3. Eliminar \n4. Sair")
+
+menu = int(input())
+
 match menu:
     case 1:
         print("Criar")
@@ -6,5 +10,6 @@ match menu:
     case 3:
         print("Eliminar")
     case 4:
+        print("")
     case _:
         print("Opção inválida!")
